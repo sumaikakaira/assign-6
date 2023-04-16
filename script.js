89744ce7fd5734ebcb072bc45eb76145f2776d4e
sumaikakaira <122632122+sumaikakaira@users.noreply.github.com>
2023-04-16 11:20:31 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//ques 1
+//assign 06
+var fruits=["apple","banana","mango","orange","stawberry"];
+document.write(fruits[0]);
+document.write(fruits[1]);
+document.write(fruits[2]);
+document.write(fruits[3]);
+document.write(fruits[4]);
